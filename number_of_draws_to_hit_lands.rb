4fb798193b01961d7ad604aba99580e74773ba63
Alex Kudlick <adkudlick@gmail.com>
2013-07-23 21:27:13 -0700

    Add a cool histogram

--- number_of_draws_to_hit_lands.rb
+++ number_of_draws_to_hit_lands.rb
@@ -28,17 +28,45 @@ class Array
   def mean
     self.inject(:+).to_f/self.length
   end
+
+  def to_histogram
+    each_with_object(Hash.new(0)) { |i, acc| acc[i] += 1 }
+  end
 end
 
-def avg_draws_to_hit_lands(num_lands, number_to_hit)
+def run_simulation(num_lands, number_to_hit)
   Array.new(1000) do
     deck = make_deck(num_lands)
     num_draws_to_hit_lands(deck, number_to_hit)
-  end.mean
+  end
+end
+
+def print_histogram histogram
+  vertical_size = histogram.values.max
+  keys = histogram.keys.sort
+
+  vertical_size.times.each do |i|
+    keys.each do |key|
+      current_height = vertical_size - i
+      value = histogram[key]
+      number_of_spaces = key.to_s.length
+      has_star = value >= current_height
+      char = has_star ? "*" : " "
+
+      number_of_spaces.times { print char }
+      print has_star ? "|" : " "
+    end
+    puts
+  end
+  keys.each {|k| print "#{k}|" }
+  puts
 end
 
 num_lands_in_deck = ARGV[0].to_i
 num_lands_to_see = ARGV[1].to_i
 
-result = avg_draws_to_hit_lands(num_lands_in_deck, num_lands_to_see)
-puts "Average number of cards to see #{num_lands_to_see} lands with #{num_lands_in_deck} lands in deck: #{result}"
+results = run_simulation num_lands_in_deck, num_lands_to_see
+
+puts "Average number of cards to see #{num_lands_to_see} lands with #{num_lands_in_deck} lands in deck: #{results.mean}"
+puts "Histogram: "
+print_histogram results.to_histogram
